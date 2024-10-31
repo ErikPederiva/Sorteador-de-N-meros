4ce992e8ded70120ece6ff27112652fce5f43bee
ErikPederiva <erikpederiva@gmail.com>
2024-10-31 13:07:33 -0300

    repositório-sorteador-de-números

---
+++ scripts.js
@@ -0,0 +1,30 @@
+    
+
+
+let raffleButton = document.querySelector("#raffle-button")
+let resultNumber = document.querySelector(".result-number")
+let resultDisplay = document.querySelector(".result-display")
+
+let detectInputMin = document.querySelector("#input-min")
+let detectInputMax = document.querySelector("#input-max")
+
+detectInputMin, detectInputMax.addEventListener("keyup", enableButton)
+function enableButton() {
+    raffleButton.disabled = false
+}
+
+raffleButton.addEventListener("click", raffle)
+function raffle() {
+
+    let min = Math.ceil(document.querySelector("#input-min").value)
+    let max = Math.floor(document.querySelector("#input-max").value)
+    let result = Math.floor(Math.random() * (max - min + 1)) + min
+
+    resultDisplay.style.display = "block"
+    resultNumber.innerHTML = result
+}
+
+function noContent() {
+
+    resultDisplay.style.display = "none"
+}
