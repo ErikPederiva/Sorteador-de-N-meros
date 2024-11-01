6b1d5d56a8ad323de3fc488256765afc934334d5
ErikPederiva <erikpederiva@gmail.com>
2024-11-01 08:31:42 -0300

    adicioado um alerta quando o valor mínimo for maior que o valor máximo

--- scripts.js
+++ scripts.js
@@ -13,16 +13,27 @@ function enableButton() {
     raffleButton.disabled = false
 }
 
+
 raffleButton.addEventListener("click", raffle)
 function raffle() {
 
     let min = Math.ceil(document.querySelector("#input-min").value)
     let max = Math.floor(document.querySelector("#input-max").value)
-    let result = Math.floor(Math.random() * (max - min + 1)) + min
 
-    resultDisplay.style.display = "block"
-    resultNumber.innerHTML = result
+    if (min < max ){
+        let result = Math.floor(Math.random() * (max - min + 1)) + min
+
+        resultDisplay.style.display = "block"
+        resultNumber.innerHTML = result
+    }
+
+    else {
+        alert("O valor mínimo não pode ser maior que o valor máximo")
+    }
+
 }
+
+
 
 function noContent() {
 
